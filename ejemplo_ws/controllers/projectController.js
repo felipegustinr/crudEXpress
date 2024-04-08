9d5a45da21a55516cdecdab121a20f8b05f3d6b7
Felipe Gustin <dfgustin.4560@unicesmag.edu.co>
2024-04-08 16:57:28 -0500

    Update projectController.js

--- ejemplo_ws/controllers/projectController.js
+++ ejemplo_ws/controllers/projectController.js
@@ -8,4 +8,59 @@ module.exports = {
         res.status(400).send(error);
       });
   },
+  getById(req, res) {
+    console.log(req.params.id);
+    return project
+      .findByPk(req.params.id)
+      .then((project) => {
+        console.log(project);
+        if (!project) {
+          return res.status(404).send({ message: "project Not Found" });
+        }
+        return res.status(200).send(project);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+  add(req, res) {
+    return project
+      .create({
+        title: req.body.title,
+        description: req.body.description,
+        state: req.body.state,
+      })
+      .then((project) => res.status(201).send(project))
+      .catch((error) => res.status(400).send(error));
+  },
+  update(req, res) {
+    return project
+      .findByPk(req.params.id)
+      .then((project) => {
+        if (!project) {
+          return res.status(404).send({ message: "project Not Found" });
+        }
+        return project
+          .update({
+            title: req.body.title || project.title,
+            description: req.body.description || project.description,
+            state: req.body.state || project.state,
+          })
+          .then(() => res.status(200).send(project))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+  delete(req, res) {
+    return project
+      .findByPk(req.params.id)
+      .then((project) => {
+        if (!project) {
+          return res.status(400).send({ message: "project Not Found" });
+        }
+        return project
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
 };
