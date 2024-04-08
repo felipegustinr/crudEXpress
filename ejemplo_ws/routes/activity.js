d82fa31708ccb82d4cc24ce531390d46aabf4851
Felipe Gustin <dfgustin.4560@unicesmag.edu.co>
2024-04-08 17:21:50 -0500

    Update activity.js

--- ejemplo_ws/routes/activity.js
+++ ejemplo_ws/routes/activity.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const projectController = require("../controllers").projectController;
-router.get("/", projectController.list);
-router.get('/:id', projectController.getById);
-router.post('/', projectController.add);
-router.put('/:id', projectController.update);
-router.delete('/:id', projectController.delete);
+const activityController = require("../controllers").activityController;
+router.get("/", activityController.list);
+router.get('/:id', activityController.getById);
+router.post('/', activityController.add);
+router.put('/:id', activityController.update);
+router.delete('/:id', activityController.delete);
 module.exports = router;
