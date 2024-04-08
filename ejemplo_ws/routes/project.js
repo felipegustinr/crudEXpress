c01693a6889d2d034f37cecd9e4649db4532ed09
Felipe Gustin <dfgustin.4560@unicesmag.edu.co>
2024-04-08 17:51:08 -0500

    Update project.js

--- ejemplo_ws/routes/project.js
+++ ejemplo_ws/routes/project.js
@@ -2,8 +2,13 @@ var express = require("express");
 var router = express.Router();
 const projectController = require("../controllers").projectController;
 router.get("/", projectController.list);
+router.get('/full', projectController.listFull);
 router.get('/:id', projectController.getById);
+router.get('/fullEnable', projectController.listEnableFull);
+router.get('/sql', projectController.getSQL);
 router.post('/', projectController.add);
 router.put('/:id', projectController.update);
 router.delete('/:id', projectController.delete);
 module.exports = router;
+
+
