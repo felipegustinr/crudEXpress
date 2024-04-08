b77d349e1c5fcc439483036bbd3cbc71f6b16ddb
Felipe Gustin <dfgustin.4560@unicesmag.edu.co>
2024-04-08 17:26:10 -0500

    Update index.js

--- ejemplo_ws/controllers/index.js
+++ ejemplo_ws/controllers/index.js
@@ -1,3 +1,4 @@
 const projectController = require("./projectController");
 const activityController = require("./activityController");
-module.exports = { projectController, activityController };
+const employController = require("./employController");
+module.exports = { projectController, activityController, employController };
