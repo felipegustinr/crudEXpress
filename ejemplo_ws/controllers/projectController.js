d004289ef73c176e2d013fb76d05d47ea2d4fe53
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2024-04-17 19:46:59 -0500

    Update Services

--- ejemplo_ws/controllers/projectController.js
+++ ejemplo_ws/controllers/projectController.js
@@ -130,8 +130,8 @@ module.exports = {
           },
         ],
         where: {
-          state: state === "true",
-          //state: state === 'true' ? true : false
+          //state: state === "true",
+          state: state === 'true' ? true : false
           
         },
       })
--- ejemplo_ws/routes/project.js
+++ ejemplo_ws/routes/project.js
@@ -6,7 +6,7 @@ router.get('/full', projectController.listFull);
 router.get('/:id', projectController.getById);
 router.get('/fullEnable', projectController.listEnableFull);
 router.get('/sql', projectController.getSQL);
-router.get('/proyectosE/:id_per_res/:state', projectController.enabledDisabledProjects);
+router.get('/projectEnable/:id_per_res/:state', projectController.enabledDisabledProjects);
 router.post('/', projectController.add);
 router.put('/:id', projectController.update);
 router.delete('/:id', projectController.delete);
